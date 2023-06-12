fef9a106ca3848b511d7ab1410415f9258c1218c
Ted Erickson <ted.erickson@comcast.net>
2023-06-12 09:24:13 -0600

    User functionality

--- src/main/java/com/wildfit/server/repository/VerificationTokenRepository.java
+++ src/main/java/com/wildfit/server/repository/VerificationTokenRepository.java
@@ -1,13 +1,10 @@
 package com.wildfit.server.repository;
 
-import com.wildfit.server.model.User;
 import com.wildfit.server.model.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-    VerificationToken findByUser(User user);
-
     VerificationToken findByToken(String token);
 }
