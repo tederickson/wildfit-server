f688f31dfacc5899e3a594bcc537d6a6e64d5fc0
Ted Erickson <ted.erickson@comcast.net>
2023-06-21 14:05:35 -0600

    Recipe functionality

---
+++ src/test/java/com/wildfit/server/manager/FoodControllerTest.java
@@ -0,0 +1,51 @@
+package com.wildfit.server.manager;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import com.wildfit.server.domain.FoodItemDigest;
+import com.wildfit.server.domain.SearchFoodResponse;
+import com.wildfit.server.exception.NutritionixException;
+import com.wildfit.server.exception.UserServiceException;
+import com.wildfit.server.service.NutritionixService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodControllerTest {
+    @Mock
+    private NutritionixService nutritionixService;
+
+    @InjectMocks
+    FoodController foodController;
+
+    @Test
+    void getFoodWithId() throws UserServiceException, NutritionixException {
+        final var id = "123";
+        when(nutritionixService.getFoodWithId(id)).thenReturn(FoodItemDigest.builder().build());
+
+        final var response = foodController.getFoodWithId(id);
+        assertNotNull(response);
+    }
+
+    @Test
+    void getFoodWithBarcode() throws UserServiceException, NutritionixException {
+        final var barcode = "12345";
+        when(nutritionixService.getFoodWithBarcode(barcode)).thenReturn(FoodItemDigest.builder().build());
+
+        final var response = foodController.getFoodWithBarcode(barcode);
+        assertNotNull(response);
+    }
+
+    @Test
+    void getFoodsByQuery() throws UserServiceException, NutritionixException {
+        final var description = "blah de blah, blah blah";
+
+        when(nutritionixService.getFoodsByQuery(description)).thenReturn(new SearchFoodResponse());
+
+        final var response = nutritionixService.getFoodsByQuery(description);
+        assertNotNull(response);
+    }
+}
