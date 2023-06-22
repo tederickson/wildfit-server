6afac692f5c0ddfd1fe82a76f47657a73705f1eb
Ted Erickson <ted.erickson@comcast.net>
2023-06-22 09:34:04 -0600

    Recipe functionality

--- src/main/java/com/wildfit/server/model/InstructionGroup.java
+++ src/main/java/com/wildfit/server/model/InstructionGroup.java
@@ -6,8 +6,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.List;
@@ -30,9 +28,7 @@ public class InstructionGroup {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @ManyToOne
-    @JoinColumn(name = "recipe_id", referencedColumnName = "id", nullable = false)
-    private Recipe recipe;
+    private long recipe_id; // Recipe.id
 
     private int instructionGroupNumber;
     private String name;
@@ -61,6 +57,7 @@ public class InstructionGroup {
     public String toString() {
         return "InstructionGroup{" +
                 "id=" + id +
+                ", recipe_id=" + recipe_id +
                 ", instructionGroupNumber=" + instructionGroupNumber +
                 ", name='" + name + '\'' +
                 '}';
