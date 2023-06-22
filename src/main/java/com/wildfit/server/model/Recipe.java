ada6bb7fdd750c7b1940decf9b49d1b3222ad2d5
Ted Erickson <ted.erickson@comcast.net>
2023-06-22 09:33:14 -0600

    Recipe functionality

--- src/main/java/com/wildfit/server/model/Recipe.java
+++ src/main/java/com/wildfit/server/model/Recipe.java
@@ -1,25 +1,23 @@
 package com.wildfit.server.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @Builder(setterPrefix = "with")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -43,12 +41,6 @@ public class Recipe {
     private LocalDateTime created;
     private LocalDateTime updated;
 
-    @OneToMany(mappedBy = "recipe", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private Set<InstructionGroup> instructionGroups;
-
-    @OneToMany(mappedBy = "recipe", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private Set<RecipeIngredient> recipeIngredients;
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -64,22 +56,5 @@ public class Recipe {
     @Override
     public int hashCode() {
         return java.util.Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Recipe{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", introduction='" + introduction + '\'' +
-                ", name='" + name + '\'' +
-                ", season='" + season + '\'' +
-                ", prepTimeMin=" + prepTimeMin +
-                ", cookTimeMin=" + cookTimeMin +
-                ", servingUnit='" + servingUnit + '\'' +
-                ", servingQty=" + servingQty +
-                ", created=" + created +
-                ", updated=" + updated +
-                '}';
     }
 }
