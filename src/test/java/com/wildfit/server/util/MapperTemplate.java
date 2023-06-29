e44575500d9c92ea16205cfca2078eb1e9fed07e
Ted Erickson <ted.erickson@comcast.net>
2023-06-29 09:18:43 -0600

    Recipe functionality

--- src/test/java/com/wildfit/server/util/MapperTemplate.java
+++ src/test/java/com/wildfit/server/util/MapperTemplate.java
@@ -1,20 +1,60 @@
 package com.wildfit.server.util;
 
 import java.lang.reflect.Field;
+import java.util.Set;
+import java.util.TreeSet;
 
-import com.wildfit.server.model.Recipe;
+import com.wildfit.server.domain.IngredientDigest;
+import com.wildfit.server.model.RecipeIngredient;
 
 public class MapperTemplate {
 
-    public static void transform(final Class from, String instanceName) {
+    private static Set<String> getFieldSuffixes(final Class from) {
+        final var fieldSuffixes = new TreeSet<String>();
 
         for (Field field : from.getDeclaredFields()) {
             final var fieldSuffix = field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
+            fieldSuffixes.add(fieldSuffix);
+        }
+        return fieldSuffixes;
+    }
+
+    private static String getInstanceName(final Class from) {
+        final var name = from.getSimpleName();
+
+        return name.substring(0, 1).toLowerCase() + name.substring(1);
+    }
+
+    public static void transform(final Class from, String to) {
+        final var instanceName = getInstanceName(from);
+
+        System.out.println("public static " + to + " map(" +
+                from.getSimpleName() +
+                " " + instanceName + ") {\n" +
+                "        return " + to + ".builder()");
+        for (var fieldSuffix : getFieldSuffixes(from)) {
             System.out.println(".with" + fieldSuffix + "(" + instanceName + ".get" + fieldSuffix + "())");
         }
+        System.out.println(".build();");
+        System.out.println("}");
+    }
+
+    public static void create(final Class from, String to) {
+        final var instanceName = getInstanceName(from);
+
+        System.out.println("public static " + to + " create(" +
+                from.getSimpleName() +
+                " " + instanceName + ") {\n" +
+                "        return " + to + ".builder()");
+        for (var fieldSuffix : getFieldSuffixes(from)) {
+            System.out.println(".with" + fieldSuffix + "(" + instanceName + ".get" + fieldSuffix + "())");
+        }
+        System.out.println(".build();");
+        System.out.println("}");
     }
 
     public static void main(String[] args) {
-        MapperTemplate.transform(Recipe.class, "recipe");
+        MapperTemplate.transform(RecipeIngredient.class, "IngredientDigest");
+        MapperTemplate.create(IngredientDigest.class, "RecipeIngredient");
     }
 }
