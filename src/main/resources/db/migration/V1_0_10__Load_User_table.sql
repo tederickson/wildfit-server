6bca21697f8374762c9ac4e69c1335d0d145b504
Ted Erickson <ted.erickson@comcast.net>
2023-07-02 00:12:27 -0600

    Recipe functionality

---
+++ src/main/resources/db/migration/V1_0_10__Load_User_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (create_date, email, enabled, password, status)
+VALUES ("2023-06-27", "wildfit@wildfit.prototype.com", 1, "encoded password", "P");
--- src/test/java/com/wildfit/server/load/CommonRecipe.java
+++ src/test/java/com/wildfit/server/load/CommonRecipe.java
@@ -1,13 +1,11 @@
 package com.wildfit.server.load;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
+import com.google.common.collect.Iterables;
 import com.wildfit.server.domain.FoodItemDigest;
 import com.wildfit.server.exception.UserServiceException;
 import com.wildfit.server.model.FoodItems;
-import com.wildfit.server.model.User;
-import com.wildfit.server.model.UserStatus;
 import com.wildfit.server.model.mapper.IngredientDigestMapper;
 import com.wildfit.server.repository.InstructionGroupRepository;
 import com.wildfit.server.repository.RecipeIngredientRepository;
@@ -16,7 +14,6 @@ import com.wildfit.server.repository.UserRepository;
 import com.wildfit.server.service.handler.CreateRecipeIngredientHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
 
 public abstract class CommonRecipe {
     protected static Long userId;
@@ -36,18 +33,7 @@ public abstract class CommonRecipe {
     void setUp() {
         final var users = userRepository.findByEmail(EMAIL);
 
-        if (CollectionUtils.isEmpty(users)) {
-            final var user = User.builder()
-                    .withStatus(UserStatus.FREE.getCode())
-                    .withCreateDate(LocalDate.now())
-                    .withPassword("encoded password")
-                    .withEmail(EMAIL).build();
-            final var dbUser = userRepository.save(user);
-
-            userId = dbUser.getId();
-        } else {
-            userId = users.get(0).getId();
-        }
+        userId = Iterables.getOnlyElement(users).getId();
     }
 
     // Use https://trackapi.nutritionix.com/v2/natural/nutrients to create JSON files
