3cbdd6d553bcb6d95899187cdbf23166d0ee56b8
Ted Erickson <ted.erickson@comcast.net>
2023-07-23 07:37:10 -0600

    Convert to OpenAPI 3

--- src/main/java/com/wildfit/server/configuration/SwaggerConfig.java
+++ src/main/java/com/wildfit/server/configuration/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.wildfit.server.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +9,8 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI springShopOpenAPI() {
-        final var info = new io.swagger.v3.oas.models.info.Info();
+    public OpenAPI wildfitOpenAPI() {
+        final var info = new Info();
         info.setTitle("WILDFIT API");
         info.setVersion("0.1.4");
         info.setDescription("REST API for WILDFIT application");
