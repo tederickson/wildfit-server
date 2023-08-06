2b888898c06e02f7d8110659751a10db4f1731df
Ted Erickson <ted.erickson@comcast.net>
2023-08-06 14:57:24 -0600

    test JSON serialization/deserialization

--- src/test/java/com/wildfit/server/domain/RecipeDigestTest.java
+++ src/test/java/com/wildfit/server/domain/RecipeDigestTest.java
@@ -3,6 +3,7 @@ package com.wildfit.server.domain;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static com.wildfit.server.domain.SeasonType.SPRING;
 import static com.wildfit.server.domain.SeasonType.WINTER;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,8 +11,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
 
+@JsonTest
 class RecipeDigestTest {
+    @Autowired
+    private JacksonTester<RecipeDigest> json;
+
     @Test
     public void shouldHaveANoArgsConstructor() {
         assertThat(RecipeDigest.class, hasValidBeanConstructor());
@@ -36,5 +45,28 @@ class RecipeDigestTest {
     void builder() {
         final var request = RecipeDigest.builder().withSeason(WINTER).build();
         assertEquals(WINTER, request.getSeason());
+    }
+
+    @Test
+    void jsonRead() throws Exception {
+        final var introduction = "Tuna is one of the staples in our household. \nWe eat it all the time, because " +
+                "it is simple and can be eaten for breakfast, lunch, dinner or snack.";
+
+        final var recipeDigest = RecipeDigest.builder()
+                                             .withName("Tuna salad")
+                                             .withSeason(SPRING)
+                                             .withPrepTimeMin(15)
+                                             .withCookTimeMin(0)
+                                             .withServingUnit("serving")
+                                             .withServingQty(4)
+                                             .withIntroduction(introduction)
+                                             .build();
+
+        final JsonContent<RecipeDigest> result = json.write(recipeDigest);
+        final var jsonText = result.getJson();
+
+        final RecipeDigest deserialized = json.parse(jsonText).getObject();
+
+        assertEquals(recipeDigest, deserialized);
     }
 }
