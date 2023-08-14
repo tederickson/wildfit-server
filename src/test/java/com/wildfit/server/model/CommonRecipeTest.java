606ebb23379a658a8732381d0272ec4d46ecd5d0
Ted Erickson <ted.erickson@comcast.net>
2023-08-14 07:40:37 -0600

    Refactor database

---
+++ src/test/java/com/wildfit/server/model/CommonRecipeTest.java
@@ -0,0 +1,22 @@
+package com.wildfit.server.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CommonRecipeTest {
+
+    @Test
+    void getRecipeGroup() {
+        final var recipeGroup = new RecipeGroup().setId(123L);
+        final var instruction = new Instruction()
+                .setId(3L)
+                .setText("Bob")
+                .setStepNumber(1)
+                .setRecipeGroup(recipeGroup);
+
+        assertEquals(recipeGroup, instruction.getRecipeGroup());
+        assertEquals(CommonRecipeType.INSTRUCTION.toString(), instruction.getType());
+        assertEquals(3L, instruction.getId());
+    }
+}
