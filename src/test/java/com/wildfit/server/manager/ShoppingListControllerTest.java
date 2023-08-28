957e90dfbc52eb188d27073e34cf03c8f4a6bf32
Ted Erickson <ted.erickson@comcast.net>
2023-08-28 15:34:09 -0600

    shopping list API

---
+++ src/test/java/com/wildfit/server/manager/ShoppingListControllerTest.java
@@ -0,0 +1,47 @@
+package com.wildfit.server.manager;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.wildfit.server.domain.CreateShoppingListRequest;
+import com.wildfit.server.exception.WildfitServiceException;
+import com.wildfit.server.service.ShoppingListService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ShoppingListControllerTest {
+    static final Long mealId = 123L;
+    static final String userId = "abra-cadabra";
+
+    @Mock
+    private ShoppingListService shoppingListService;
+
+    private ShoppingListController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ShoppingListController(shoppingListService);
+    }
+
+    @Test
+    void getShoppingList() throws WildfitServiceException {
+        assertNull(controller.getShoppingList(userId));
+    }
+
+    @Test
+    void deleteItemFromShoppingList() throws WildfitServiceException {
+        controller.deleteItemFromShoppingList(userId, 1319L);
+    }
+
+    @Test
+    void createShoppingList() throws WildfitServiceException {
+        CreateShoppingListRequest request = CreateShoppingListRequest.builder()
+                                                                     .withUuid(userId)
+                                                                     .withMealId(mealId)
+                                                                     .build();
+        assertNull(controller.createShoppingList(request));
+    }
+}
