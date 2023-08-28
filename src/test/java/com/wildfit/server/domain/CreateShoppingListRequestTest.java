41a31fd1ec3a29c362a9dfd7253fc687adfdb4d3
Ted Erickson <ted.erickson@comcast.net>
2023-08-27 19:38:05 -0600

    shopping list API

---
+++ src/test/java/com/wildfit/server/domain/CreateShoppingListRequestTest.java
@@ -0,0 +1,44 @@
+package com.wildfit.server.domain;
+
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.jupiter.api.Test;
+
+class CreateShoppingListRequestTest {
+
+    @Test
+    public void shouldHaveNoArgsConstructor() {
+        assertThat(CreateShoppingListRequest.class, hasValidBeanConstructor());
+    }
+
+    @Test
+    public void gettersAndSettersShouldWorkForEachProperty() {
+        assertThat(CreateShoppingListRequest.class, hasValidGettersAndSetters());
+    }
+
+    @Test
+    public void allPropertiesShouldBeRepresentedInToStringOutput() {
+        assertThat(CreateShoppingListRequest.class, hasValidBeanToString());
+    }
+
+    @Test
+    public void equalsAndHashCode() {
+        EqualsVerifier.forClass(CreateShoppingListRequest.class).suppress(Warning.NONFINAL_FIELDS).verify();
+    }
+
+    @Test
+    void builder() {
+        final var request = CreateShoppingListRequest.builder()
+                                                     .withUuid("unique user id")
+                                                     .build();
+        assertEquals("unique user id", request.getUuid());
+        assertNull(request.getMealId());
+    }
+}
