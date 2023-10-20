fec2b4965a507129ba8fb2a3cd1606da9d93c5f1
Ted Erickson <ted.erickson@comcast.net>
2023-10-20 07:21:10 -0600

    shopping list API

---
+++ src/test/java/com/wildfit/server/domain/ShoppingListDigestTest.java
@@ -0,0 +1,50 @@
+package com.wildfit.server.domain;
+
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.jupiter.api.Test;
+
+class ShoppingListDigestTest {
+
+    @Test
+    public void shouldHaveNoArgsConstructor() {
+        assertThat(ShoppingListDigest.class, hasValidBeanConstructor());
+    }
+
+    @Test
+    public void gettersAndSettersShouldWorkForEachProperty() {
+        assertThat(ShoppingListDigest.class, hasValidGettersAndSetters());
+    }
+
+    @Test
+    public void allPropertiesShouldBeRepresentedInToStringOutput() {
+        assertThat(ShoppingListDigest.class, hasValidBeanToString());
+    }
+
+    @Test
+    public void equalsAndHashCode() {
+        EqualsVerifier.forClass(ShoppingListDigest.class).suppress(Warning.NONFINAL_FIELDS).verify();
+    }
+
+    @Test
+    void builder() {
+        final var digest = ShoppingListDigest.builder()
+                                             .withId(142143L)
+                                             .withUuid("APP")
+                                             .withItems(List.of())
+                                             .build();
+
+        assertEquals(142143L, digest.getId());
+        assertEquals("APP", digest.getUuid());
+        assertTrue(digest.getItems().isEmpty());
+    }
+}
