cef26e25bc4b01145728945eea66c813da6e0336
Ted Erickson <ted.erickson@comcast.net>
2023-10-21 06:26:29 -0600

    shopping list API

---
+++ src/test/java/com/wildfit/server/model/ShoppingListTest.java
@@ -0,0 +1,32 @@
+package com.wildfit.server.model;
+
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.jupiter.api.Test;
+
+class ShoppingListTest {
+    @Test
+    public void shouldHaveNoArgsConstructor() {
+        assertThat(ShoppingList.class, hasValidBeanConstructor());
+    }
+
+    @Test
+    public void toStringOutput() {
+        var shoppingList = new ShoppingList();
+        assertNotNull(shoppingList.toString());
+    }
+
+    @Test
+    public void equalsAndHashCode() {
+        EqualsVerifier.simple().forClass(ShoppingList.class)
+                      .withPrefabValues(ShoppingListItem.class,
+                              new ShoppingListItem().setId(3L),
+                              new ShoppingListItem().setId(13L))
+                      .suppress(Warning.NONFINAL_FIELDS)
+                      .suppress(Warning.SURROGATE_KEY).verify();
+    }
+}
