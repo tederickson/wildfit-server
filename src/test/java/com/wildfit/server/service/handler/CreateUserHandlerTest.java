fad8fba14e1caf634de892a55733bffd3416972a
Ted Erickson <ted.erickson@comcast.net>
2024-10-30 09:12:09 -0600

    Test code coverage

--- src/test/java/com/wildfit/server/service/handler/CreateUserHandlerTest.java
+++ src/test/java/com/wildfit/server/service/handler/CreateUserHandlerTest.java
@@ -4,15 +4,10 @@ import com.wildfit.server.exception.WildfitServiceError;
 import com.wildfit.server.exception.WildfitServiceException;
 import com.wildfit.server.model.User;
 import com.wildfit.server.model.UserStatus;
-import com.wildfit.server.repository.UserProfileRepository;
-import com.wildfit.server.repository.VerificationTokenRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.mail.javamail.JavaMailSender;
 
 import java.util.UUID;
 
@@ -22,14 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class CreateUserHandlerTest extends CommonHandlerTest {
-    @Autowired
-    UserProfileRepository userProfileRepository;
-    @Autowired
-    VerificationTokenRepository verificationTokenRepository;
-    @Autowired
-    Environment environment;
-    @Autowired
-    JavaMailSender javaMailSender;
 
     @Test
     void nullParameters() {
@@ -41,32 +28,14 @@ class CreateUserHandlerTest extends CommonHandlerTest {
     @NullAndEmptySource
     void nullPassword(String password) {
         final var exception = assertThrows(WildfitServiceException.class,
-                                           () -> CreateUserHandler.builder()
-                                                   .withUserRepository(userRepository)
-                                                   .withUserProfileRepository(userProfileRepository)
-                                                   .withVerificationTokenRepository(verificationTokenRepository)
-                                                   .withEnvironment(environment)
-                                                   .withJavaMailSender(javaMailSender)
-                                                   .withEmail(EMAIL)
-                                                   .withPassword(password)
-                                                   .withName(NAME)
-                                                   .build().execute());
+                                           () -> userService.createUser(EMAIL, password, NAME));
         assertEquals(WildfitServiceError.INVALID_PASSWORD, exception.getError());
     }
 
     @Test
     void invalidPassword() {
         final var exception = assertThrows(WildfitServiceException.class,
-                                           () -> CreateUserHandler.builder()
-                                                   .withUserRepository(userRepository)
-                                                   .withUserProfileRepository(userProfileRepository)
-                                                   .withVerificationTokenRepository(verificationTokenRepository)
-                                                   .withEnvironment(environment)
-                                                   .withJavaMailSender(javaMailSender)
-                                                   .withPassword("apple")
-                                                   .withEmail(EMAIL)
-                                                   .withName(NAME)
-                                                   .build().execute());
+                                           () -> userService.createUser(EMAIL, "apple", NAME));
         assertEquals(WildfitServiceError.INVALID_PASSWORD, exception.getError());
     }
 
@@ -74,32 +43,14 @@ class CreateUserHandlerTest extends CommonHandlerTest {
     @NullAndEmptySource
     void missingEmail(String email) {
         final var exception = assertThrows(WildfitServiceException.class,
-                                           () -> CreateUserHandler.builder()
-                                                   .withUserRepository(userRepository)
-                                                   .withUserProfileRepository(userProfileRepository)
-                                                   .withVerificationTokenRepository(verificationTokenRepository)
-                                                   .withEnvironment(environment)
-                                                   .withJavaMailSender(javaMailSender)
-                                                   .withPassword(PASSWORD)
-                                                   .withEmail(email)
-                                                   .withName(NAME)
-                                                   .build().execute());
+                                           () -> userService.createUser(email, PASSWORD, NAME));
         assertEquals(WildfitServiceError.MISSING_EMAIL, exception.getError());
     }
 
     @Test
     void emptyEmail() {
         final var exception = assertThrows(WildfitServiceException.class,
-                                           () -> CreateUserHandler.builder()
-                                                   .withUserRepository(userRepository)
-                                                   .withUserProfileRepository(userProfileRepository)
-                                                   .withVerificationTokenRepository(verificationTokenRepository)
-                                                   .withEnvironment(environment)
-                                                   .withJavaMailSender(javaMailSender)
-                                                   .withPassword(PASSWORD)
-                                                   .withEmail("    ")
-                                                   .withName(NAME)
-                                                   .build().execute());
+                                           () -> userService.createUser("    ", PASSWORD, NAME));
         assertEquals(WildfitServiceError.MISSING_EMAIL, exception.getError());
     }
 
@@ -107,47 +58,20 @@ class CreateUserHandlerTest extends CommonHandlerTest {
     @NullAndEmptySource
     void missingName(String name) {
         final var exception = assertThrows(WildfitServiceException.class,
-                                           () -> CreateUserHandler.builder()
-                                                   .withUserRepository(userRepository)
-                                                   .withUserProfileRepository(userProfileRepository)
-                                                   .withVerificationTokenRepository(verificationTokenRepository)
-                                                   .withEnvironment(environment)
-                                                   .withJavaMailSender(javaMailSender)
-                                                   .withPassword(PASSWORD)
-                                                   .withEmail(EMAIL)
-                                                   .withName(name)
-                                                   .build().execute());
+                                           () -> userService.createUser(EMAIL, PASSWORD, name));
         assertEquals(WildfitServiceError.INVALID_NAME, exception.getError());
     }
 
     @Test
     void emptyName() {
         final var exception = assertThrows(WildfitServiceException.class,
-                                           () -> CreateUserHandler.builder()
-                                                   .withUserRepository(userRepository)
-                                                   .withUserProfileRepository(userProfileRepository)
-                                                   .withVerificationTokenRepository(verificationTokenRepository)
-                                                   .withEnvironment(environment)
-                                                   .withJavaMailSender(javaMailSender)
-                                                   .withPassword(PASSWORD)
-                                                   .withEmail(EMAIL)
-                                                   .withName("    ")
-                                                   .build().execute());
+                                           () -> userService.createUser(EMAIL, PASSWORD, "    "));
         assertEquals(WildfitServiceError.INVALID_NAME, exception.getError());
     }
 
     @Test
     void execute() throws WildfitServiceException {
-        final var response = CreateUserHandler.builder()
-                .withUserRepository(userRepository)
-                .withUserProfileRepository(userProfileRepository)
-                .withVerificationTokenRepository(verificationTokenRepository)
-                .withEnvironment(environment)
-                .withJavaMailSender(javaMailSender)
-                .withPassword(PASSWORD)
-                .withEmail(EMAIL)
-                .withName(NAME)
-                .build().execute();
+        final var response = userService.createUser(EMAIL, PASSWORD, NAME);
 
         assertEquals(EMAIL, response.getEmail());
 
@@ -167,18 +91,8 @@ class CreateUserHandlerTest extends CommonHandlerTest {
         assertNotNull(saved);
 
         final var exception = assertThrows(WildfitServiceException.class,
-                                           () -> CreateUserHandler.builder()
-                                                   .withUserRepository(userRepository)
-                                                   .withUserProfileRepository(userProfileRepository)
-                                                   .withVerificationTokenRepository(verificationTokenRepository)
-                                                   .withEnvironment(environment)
-                                                   .withJavaMailSender(javaMailSender)
-                                                   .withPassword(PASSWORD)
-                                                   .withEmail(EMAIL)
-                                                   .withName(NAME)
-                                                   .build().execute());
+                                           () -> userService.createUser(EMAIL, PASSWORD, NAME));
 
         assertEquals(WildfitServiceError.EXISTING_USER, exception.getError());
     }
-
 }
